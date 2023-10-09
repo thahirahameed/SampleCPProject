98e6d289122df864df44002425b4d48fd9421a8d
thahirahameed <95245821+thahirahameed@users.noreply.github.com>
2023-10-09 10:58:17 +0100

    added login and dashboard

--- screens/DashboardScreen/index.js
+++ screens/DashboardScreen/index.js
@@ -1,8 +1,12 @@
-import {} from 'react';
+import {useState} from 'react';
 import {Button, Text, TouchableOpacity, View} from 'react-native';
 import {useUserContext} from '../../contexts/UserContext';
 
 const DashboardScreen = () => {
+  const [content, setContent] = useState(
+    '\nWelcome. We are very happy to have you with us. Here is a little more information about who we are, and a few links about our story and success cases. We hope you find in our business what you are looking for. ',
+  );
+
   const {
     state: {data},
     actions: {setData},
@@ -10,20 +14,7 @@ const DashboardScreen = () => {
 
   return (
     <View>
-      <Text>Welcome Back!! You are logged in! </Text>
-
-      <Text
-        style={{
-          backgroundColor: 'lightgrey',
-          height: 40,
-          margin: 10,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-        {data}
-      </Text>
-
-      <View>
+      <View style={{marginStart: 150, marginBottom: 50}}>
         <TouchableOpacity
           style={{
             height: 40,
@@ -39,6 +30,19 @@ const DashboardScreen = () => {
           <Text>LOGOUT</Text>
         </TouchableOpacity>
       </View>
+
+      <Text>Welcome Back {data} !! You are logged in! </Text>
+
+      <Text
+        style={{
+          backgroundColor: 'lightgrey',
+          height: 100,
+          margin: 10,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        {content}
+      </Text>
     </View>
   );
 };
--- screens/LoginScreen/index.js
+++ screens/LoginScreen/index.js
@@ -1,5 +1,12 @@
 import {useState} from 'react';
-import {Text, View, TextInput, TouchableOpacity} from 'react-native';
+import {
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+  ImageBackground,
+} from 'react-native';
 import {useUserContext} from '../../contexts/UserContext';
 import Navigator from '../../navigation';
 
@@ -12,52 +19,83 @@ const LoginScreen = props => {
     actions: {setData},
   } = useUserContext();
 
+  const validateEmail = email => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const handleValidation = () => {
+    if (validateEmail(email)) {
+      return true;
+    } else {
+      Alert.alert('Invalid Email', 'Please enter a valid email address.');
+      return false;
+    }
+  };
+
   return (
     <View style={{flex: 1}}>
-      <Text>Login</Text>
-
-      <TextInput
-        value={email}
-        onChangeText={changedText => {
-          setEmail(changedText);
-        }}
-        placeholder="Email"
+      <ImageBackground
+        source={'https://picsum.photos/200/300'}
         style={{
-          backgroundColor: 'lightgrey',
-          padding: 10,
-          margin: 10,
-          height: 40,
-        }}
-      />
-      <TextInput
-        value={password}
-        onChangeText={changedText => {
-          setPassword(changedText);
-        }}
-        secureTextEntry
-        placeholder="Password"
-        style={{
-          backgroundColor: 'lightgrey',
-          padding: 10,
-          margin: 10,
-          height: 40,
-        }}
-      />
-
-      <TouchableOpacity
-        onPress={() => {
-          setData(email);
-          props.navigation.navigate('dashboardScreen');
-        }}
-        style={{
-          marginHorizontal: 10,
-          height: 40,
+          flex: 1,
+          resizeMode: 'cover',
           justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: 'lightblue',
         }}>
-        <Text>LOGIN</Text>
-      </TouchableOpacity>
+        <View>
+          <Text style={{fontSize: 40, color: 'green', fontWeight: 'bold'}}>
+            Welcome to the App
+          </Text>
+          <Text style={{fontSize: 30, color: 'blue', fontWeight: 'bold'}}>
+            Please Login To Enter
+          </Text>
+        </View>
+        <TextInput
+          value={email}
+          onChangeText={changedText => {
+            setEmail(changedText);
+          }}
+          placeholder="Email"
+          style={{
+            backgroundColor: 'lightgrey',
+            padding: 10,
+            margin: 10,
+            height: 40,
+          }}
+        />
+
+        <TextInput
+          value={password}
+          onChangeText={changedText => {
+            setPassword(changedText);
+          }}
+          secureTextEntry
+          placeholder="Password"
+          style={{
+            backgroundColor: 'lightgrey',
+            padding: 10,
+            margin: 10,
+            height: 40,
+          }}
+        />
+
+        <TouchableOpacity
+          onPress={() => {
+            if (handleValidation(email)) {
+              setData(email);
+              props.navigation.navigate('dashboardScreen');
+            }
+          }}
+          style={{
+            marginHorizontal: 10,
+            height: 40,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'lightblue',
+          }}>
+          <Text>LOGIN</Text>
+        </TouchableOpacity>
+      </ImageBackground>
     </View>
   );
 };
